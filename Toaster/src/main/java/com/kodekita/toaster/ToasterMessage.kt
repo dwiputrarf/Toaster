b83046a9125f6c7c3e79207504635a05a141e2f9
Rizky Febrianto D <rizkyfebriantodwiputra@gmail.com>
2022-12-13 09:33:55 +0700

    test

--- Toaster/src/main/java/com/kodekita/toaster/ToasterMessage.kt
+++ Toaster/src/main/java/com/kodekita/toaster/ToasterMessage.kt
@@ -3,8 +3,8 @@ package com.kodekita.toaster
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
-    fun toast(context: Context?, message: String?) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+object ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
