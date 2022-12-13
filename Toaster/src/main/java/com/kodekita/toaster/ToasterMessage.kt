3077cb294aa833d26c084389bc0f651c15bb0063
Rizky Febrianto D <rizkyfebriantodwiputra@gmail.com>
2022-12-13 10:10:14 +0700

    add dependency to module app

--- Toaster/src/main/java/com/kodekita/toaster/ToasterMessage.kt
+++ Toaster/src/main/java/com/kodekita/toaster/ToasterMessage.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 object ToasterMessage {
-    fun s(c: Context?, message: String?) {
+    fun toaster(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
